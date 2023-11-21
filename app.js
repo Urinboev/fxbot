792fd5fb53a4730863360d790064e6a5b4f1f1cd
Urinboev <60550342+Urinboev@users.noreply.github.com>
2023-11-21 15:22:14 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js").then(reg => {
+    console.log("sw registered");
+    console.log(reg);
+  }).catch(error => {
+    console.log(error)
+  };
+}
