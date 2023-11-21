71ca6250312f30eddfa2ff4cd8e3053e0a9769ea
firdavsorinboyevv@gmail.com <firdavsorinboyevv@gmail.com>
2023-11-21 23:21:03 +0500

    update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,74 @@
-self.addEventListener("install", (e) => {
-  console.log("installed");
+let cacheName = 'fxbot'
+let files = [
+  './app.js',
+  './black-binance-usd-busd-logo.png'
+]
+
+self.addEventListener('install', (event) => {
+  console.info('Event: Install');
+
+  event.waitUntil(
+    caches.open(cacheName)
+    .then((cache) => {
+      //[] of files to cache & if any of the file not present `addAll` will fail
+      return cache.addAll(files)
+      .then(() => {
+        console.info('All files are cached');
+        return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
+      })
+      .catch((error) =>  {
+        console.error('Failed to cache', error);
+      })
+    })
+  );
 });
 
-self.addEventListener("activate", (e) => {
-  console.log("activated");
+/*
+  FETCH EVENT: triggered for every request made by index page, after install.
+*/
+
+//Adding `fetch` event listener
+self.addEventListener('fetch', (event) => {
+  console.info('Event: Fetch');
+
+  // // Checking for navigation preload response
+  // if (event.preloadResponse) {
+  //   console.info('Using navigation preload');
+  //   return response;
+  // }
+});
+self.addEventListener('activate', (event) => {
+  console.info('Event: Activate');
+
+  //Navigation preload is help us make parallel request while service worker is booting up.
+  //Enable - chrome://flags/#enable-service-worker-navigation-preload
+  //Support - Chrome 57 beta (behing the flag)
+  //More info - https://developers.google.com/web/updates/2017/02/navigation-preload#the-problem
+
+  // Check if navigationPreload is supported or not
+  // if (self.registration.navigationPreload) { 
+  //   self.registration.navigationPreload.enable();
+  // }
+  // else if (!self.registration.navigationPreload) { 
+  //   console.info('Your browser does not support navigation preload.');
+  // }
+
+  //Remove old and unwanted caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            return caches.delete(cache); //Deleting the old cache (cache v1)
+          }
+        })
+      );
+    })
+    .then(function () {
+      console.info("Old caches are cleared!");
+      // To tell the service worker to activate current one 
+      // instead of waiting for the old one to finish.
+      return self.clients.claim(); 
+    }) 
+  );
 });
