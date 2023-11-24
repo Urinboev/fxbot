16e6624f3bf9c02f2a23a040881f8e2e77585f83
firdavsorinboyevv@gmail.com <firdavsorinboyevv@gmail.com>
2023-11-24 22:31:39 +0500

    commit

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js").then(reg => {
-    console.log("sw registered");
-    console.log(reg);
-  }).catch(error => {
-    console.log(error)
-  });
-}
+const pairs = [
+  "ANKRUSDT",
+  // "ETHUSDT",
+  // "LTCUSDT",
+  // "TRXUSDT",
+  // "LINKUSDT",
+  // "ADAUSDT",
+  // "BNBUSDT",
+  // "ATOMUSDT",
+  // "IOTAUSDT",
+  // "VETUSDT",
+  // "IOSTUSDT",
+  // "DOGEUSDT",
+  // "RLCUSDT",
+  // "SOLUSDT",
+  // "ICXUSDT",
+  // "UNIUSDT",
+  // "FTMUSDT",
+  // "MATICUSDT",
+  // "REEFUSDT",
+  // "1INCHUSDT",
+];
+
+// async function makeSignal() {
+//   let signals = await all("signals");
+
+//   for (let i = 0; i < pairs.length; i++) {
+//     fetch(
+//       "https://fapi.binance.com/fapi/v1/klines?symbol=" +
+//         pairs[i] +
+//         "&interval=1m&limit=300"
+//     )
+//       .then((response) => response.json())
+//       .then(async (json) => {
+//         console.log(pairs[i]);
+//       });
+//   }
+// }
+// await makeSignal();
